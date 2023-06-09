659ebe345a40ab9869581a3f81d99145853971fa
modeco80 <lily.modeco80@protonmail.ch>
2023-06-08 23:23:58 -0400

    prototype of cpu mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+
+
+mod cpu {
+
+    const NUM_GPR: usize = 32;
+
+    struct Fpcsr(u32);
+
+    impl Fpcsr {
+
+        ///
+        fn as_u32(&self) -> u32 {
+            return self.0;
+        }
+
+    }
+
+    struct Cpu {
+        gpr: [u32; NUM_GPR],
+        fpr: [f64; NUM_GPR],
+
+        fpcsr: Fpcsr
+
+    }
+
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
